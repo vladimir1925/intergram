bf65c08f65653640ee833c29a788510905107917
vladimir1925 <96591272+vladimir1925@users.noreply.github.com>
2022-02-08 15:06:41 +0800

    Update message-area.js

--- src/chat/message-area.js
+++ src/chat/message-area.js
@@ -19,7 +19,7 @@ export default class MessageArea extends Component {
             <ol class="chat">
                 {props.messages.map(({name, text, from, time}) => {
                     if (from === 'visitor') {
-                        name = "You";
+                        name = "IKAW";
                     }
                     return (
                         <li class={from}>
